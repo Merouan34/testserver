dbe42934129f94b7a9addcaa647354f5f537e710
Merouan <merouanmeneu34@gmail.com>
2024-05-15 14:54:18 +0200

    Set up a Netlify site with 2 serverless functions.

---
+++ netlify/functions/wpapi.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+
+export const handler = async () => {
+    const POKE_API = 'https://nsfestival.fr/wp-json/wp/v2/'
+  
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data
+      })
+    }
+  }
